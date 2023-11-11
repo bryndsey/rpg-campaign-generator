92a06b0d67d337f0dcd4ef74522f3866ee605d3e
Bryan <blinz117@gmail.com>
2023-11-10 20:30:49 -0600

    Add simple svelte component for displaying randomized name

---
+++ src/Example.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { getRandomName } from "./data";
+
+  let currentName = getRandomName();
+
+  function getNewName() {
+    currentName = getRandomName();
+  }
+</script>
+
+<div>
+  <h1>Hello, {currentName}!</h1>
+  <button on:click={getNewName}>Get new name</button>
+</div>
