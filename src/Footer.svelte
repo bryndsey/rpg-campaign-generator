c68640d59658c5993a7b532173409145df071d96
Bryan <blinz117@gmail.com>
2024-01-01 13:53:51 -0600

    Balance footer text

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="footer footer-center p-4">
   <aside>
-    <p class="whitespace-normal text-xs opacity-80">
+    <p class="whitespace-normal text-balance text-xs opacity-80">
       RPG Campaign Generator is meant to be used merely as a source of
       inspriation, and makes no copyright claims or warranties on the content
       generated. The content may not be entirely original or appropriate. Use at
