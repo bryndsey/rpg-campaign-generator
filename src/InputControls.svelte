88d3a0c1475527fa83492aa1eb0d3cf45fdc37c2
Bryan <blinz117@gmail.com>
2024-01-08 19:59:11 -0600

    Fix issue with focus ring being clipped
    For some reason, the ring gets clipped, even when using what I think should be the correct settings to not do that. I couldn't figure out why it was happening though, so kinda just hacking around it by adding some padding and then a border to try to motivate the padding a little better.

--- src/InputControls.svelte
+++ src/InputControls.svelte
@@ -6,43 +6,49 @@
 </script>
 
 <div class="flex h-full flex-col overflow-clip landscape:md:gap-4">
-  <h2 class="font-semibold landscape:text-lg">
-    Settings <span class="text-xs font-normal opacity-80">(optional)</span>
-  </h2>
   <div
-    class="flex flex-row gap-2 overflow-y-auto portrait:flex-wrap landscape:flex-1 landscape:flex-col landscape:md:gap-4"
+    class="card card-compact overflow-y-auto border-2 border-base-content/5 landscape:flex-1"
   >
-    <label class="form-control w-full portrait:flex-1">
-      <div class="label">
-        <span class="label-text">Tone</span>
-      </div>
-      <select
-        class="select select-bordered select-sm landscape:md:select-md"
-        disabled={$state.state === "loading"}
-        bind:value={$tone}
+    <div class="card-body">
+      <h2 class="font-semibold landscape:text-lg">
+        Settings <span class="text-xs font-normal opacity-80">(optional)</span>
+      </h2>
+      <div
+        class="flex flex-row gap-2 portrait:flex-wrap landscape:flex-col landscape:md:gap-4"
       >
-        {#each tones as tone}
-          <option>{tone}</option>
-        {/each}
-      </select>
-    </label>
+        <label class="form-control w-full portrait:flex-1">
+          <div class="label">
+            <span class="label-text">Tone</span>
+          </div>
+          <select
+            class="select select-bordered select-sm landscape:md:select-md"
+            disabled={$state.state === "loading"}
+            bind:value={$tone}
+          >
+            {#each tones as tone}
+              <option>{tone}</option>
+            {/each}
+          </select>
+        </label>
 
-    <label class="form-control portrait:flex-1">
-      <div class="label">
-        <span class="label-text">Topic</span>
-        <span class="label-text-alt"
-          >{`${$topic?.length ?? 0}/${MAX_INPUT_CHARACTERS}`}</span
-        >
+        <label class="form-control portrait:flex-1">
+          <div class="label">
+            <span class="label-text">Topic</span>
+            <span class="label-text-alt"
+              >{`${$topic?.length ?? 0}/${MAX_INPUT_CHARACTERS}`}</span
+            >
+          </div>
+          <input
+            bind:value={$topic}
+            type="text"
+            class="input input-bordered select-sm max-w-prose landscape:md:select-md placeholder:opacity-60"
+            disabled={$state.state === "loading"}
+            maxlength={MAX_INPUT_CHARACTERS}
+            placeholder="e.g. 'Dragon', 'Yellow', 'Tuesday', etc."
+          />
+        </label>
       </div>
-      <input
-        bind:value={$topic}
-        type="text"
-        class="input input-bordered select-sm max-w-prose landscape:md:select-md placeholder:opacity-60"
-        disabled={$state.state === "loading"}
-        maxlength={MAX_INPUT_CHARACTERS}
-        placeholder="e.g. 'Dragon', 'Yellow', 'Tuesday', etc."
-      />
-    </label>
+    </div>
   </div>
   <button
     class="btn btn-secondary mt-4 w-full"
