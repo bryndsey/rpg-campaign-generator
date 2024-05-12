b1219627d76b83eac3e6b86772c9e26df27c1451
Bryan <blinz117@gmail.com>
2024-05-12 13:31:06 -0500

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [svelte(), tailwind()],
-  adapter: vercel()
+  integrations: [svelte(), react(), tailwind()],
+  adapter: vercel(),
 });
